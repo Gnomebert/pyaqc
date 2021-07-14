2e836a517500cb2ae45137a74c0ce0329e8c85b5
Gnomebert <56593591+Gnomebert@users.noreply.github.com>
2021-07-14 12:01:26 +0100

    update OpenListTxt()

--- RCModules/PlatformConversion.py
+++ RCModules/PlatformConversion.py
@@ -47,6 +47,40 @@ def Adjaceny_to_DWave_ising( qubo: 'Adjaceny table in dict form: qubo[(r,c)]', n
             if c > r :
                 quadratic[(r,c)]        = qubo[(r,c)] 
     return linear, quadratic
+def OpenListTxt(filename, decimal_places=3):
+    """
+    filename is presumed to be made up of a list containing numbers and lists
+    """
+    f = open(filename, "r+")        #does not erase file just reads it.
+    DataOut = []
+    sub_list = []
+    liststr = ''
+    string = f.readlines()              #read all the lines in the file
+                                     # line by line  and create a list using the space delimiter
+    for n, line in enumerate(string):
+        #print(line, 'line')#, end='')
+        line.rstrip()
+        list_of_new_line = []
+        listfnd = 0
+        for x in line.split(' '):  #A)strip end white space B) split the string by the delimiter C) convert to float list:
+            if x != '\n':
+                if  x.rfind('[')>=0 or listfnd  > 0 :   #start of a sub_list
+                    
+                    listfnd += 1            # ADD number of [
+                    y = x.strip('[')
+                    y = y.strip(']')
+                    y = y.strip(',')
+                    sub_list.append( float(('%3.'+str(decimal_places)+'f')% float(y)) )
+                    if x.rfind(']') >=0:                #end of a sub_list
+                        listfnd -= 1        # SUB number of ]
+                        list_of_new_line.append(sub_list)
+                        sub_list = []
+                else:
+                    list_of_new_line.append( float(('%3.'+str(decimal_places)+'f')% float(x)) )
+            else:
+                DataOut.append( list_of_new_line)
+    f.close()
+    return DataOut
 
 def SaveListTxt(filename: '.txt file', ListToSave: 'A list of lists of results for each starting betagamma'):
     """
